c60acad2007e960f583002d49924003a8b2534c2
Danya <daniltretacenko@Mac-mini-Danil.local>
2025-10-24 23:36:02 +0300

    Sprint_7

--- MovieQuiz/Services/QuestionFactory.swift
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -10,13 +10,13 @@ import Foundation
 class QuestionFactory: QuestionFactoryProtocol {
     weak var delegate: QuestionFactoryDelegate?
     
+    private var movies: [MostPopularMovie] = []
+    private let moviesLoader: MoviesLoading
+    
     init(moviesLoader: MoviesLoading, delegate: QuestionFactoryDelegate?) {
         self.moviesLoader = moviesLoader
         self.delegate = delegate
     }
-    
-    private var movies: [MostPopularMovie] = []
-    private let moviesLoader: MoviesLoading
     
     func loadData() {
         moviesLoader.loadMovies { [weak self] result in
@@ -57,60 +57,62 @@ class QuestionFactory: QuestionFactoryProtocol {
             let text = "Рейтинг этого фильма больше чем 7?"
             let correctAnswer = rating > 7
             
-            let question = QuizQuestion(image: imageData,
-                                        text: text,
-                                        correctAnswer: correctAnswer)
+            let question = QuizQuestion(
+                image: imageData,
+                text: text,
+                correctAnswer: correctAnswer
+            )
             
             DispatchQueue.main.async { [weak self] in
                 guard let self = self else { return }
-                self.delegate?.didReceiveNextQuestion(question: question)
+                self.delegate?.didRecieveNextQuestion(question: question)
             }
         }
     }
-    
-    //    private let questions: [QuizQuestion] = [
-    //        QuizQuestion(
-    //            image: "The Godfather",
-    //            text: "Рейтинг этого фильма больше чем 6?",
-    //            correctAnswer: true),
-    //        QuizQuestion(
-    //            image: "The Dark Knight",
-    //            text: "Рейтинг этого фильма больше чем 6?",
-    //            correctAnswer: true),
-    //        QuizQuestion(
-    //            image: "Kill Bill",
-    //            text: "Рейтинг этого фильма больше чем 6?",
-    //            correctAnswer: true),
-    //        QuizQuestion(
-    //            image: "The Avengers",
-    //            text: "Рейтинг этого фильма больше чем 6?",
-    //            correctAnswer: true),
-    //        QuizQuestion(
-    //            image: "Deadpool",
-    //            text: "Рейтинг этого фильма больше чем 6?",
-    //            correctAnswer: true),
-    //        QuizQuestion(
-    //            image: "The Green Knight",
-    //            text: "Рейтинг этого фильма больше чем 6?",
-    //            correctAnswer: true),
-    //        QuizQuestion(
-    //            image: "Old",
-    //            text: "Рейтинг этого фильма больше чем 6?",
-    //            correctAnswer: false),
-    //        QuizQuestion(
-    //            image: "The Ice Age Adventures of Buck Wild",
-    //            text: "Рейтинг этого фильма больше чем 6?",
-    //            correctAnswer: false),
-    //        QuizQuestion(
-    //            image: "Tesla",
-    //            text: "Рейтинг этого фильма больше чем 6?",
-    //            correctAnswer: false),
-    //        QuizQuestion(
-    //            image: "Vivarium",
-    //            text: "Рейтинг этого фильма больше чем 6?",
-    //            correctAnswer: false)
-    //    ]
-    
-    
 }
+
+//    private let questions: [QuizQuestion] = [
+//        QuizQuestion(
+//            image: "The Godfather",
+//            text: "Рейтинг этого фильма больше чем 6?",
+//            correctAnswer: true),
+//        QuizQuestion(
+//            image: "The Dark Knight",
+//            text: "Рейтинг этого фильма больше чем 6?",
+//            correctAnswer: true),
+//        QuizQuestion(
+//            image: "Kill Bill",
+//            text: "Рейтинг этого фильма больше чем 6?",
+//            correctAnswer: true),
+//        QuizQuestion(
+//            image: "The Avengers",
+//            text: "Рейтинг этого фильма больше чем 6?",
+//            correctAnswer: true),
+//        QuizQuestion(
+//            image: "Deadpool",
+//            text: "Рейтинг этого фильма больше чем 6?",
+//            correctAnswer: true),
+//        QuizQuestion(
+//            image: "The Green Knight",
+//            text: "Рейтинг этого фильма больше чем 6?",
+//            correctAnswer: true),
+//        QuizQuestion(
+//            image: "Old",
+//            text: "Рейтинг этого фильма больше чем 6?",
+//            correctAnswer: false),
+//        QuizQuestion(
+//            image: "The Ice Age Adventures of Buck Wild",
+//            text: "Рейтинг этого фильма больше чем 6?",
+//            correctAnswer: false),
+//        QuizQuestion(
+//            image: "Tesla",
+//            text: "Рейтинг этого фильма больше чем 6?",
+//            correctAnswer: false),
+//        QuizQuestion(
+//            image: "Vivarium",
+//            text: "Рейтинг этого фильма больше чем 6?",
+//            correctAnswer: false)
+//    ]
+
+
 
